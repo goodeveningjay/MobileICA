5ab3568cde2ad18d2467b9e6c046d265736b17c4
Jay Bunch <w9055613@live.tees.ac.uk>
2023-03-27 11:55:11 +0100

    Gesture Implementation Started

--- app/src/main/java/tees/w9055613/mymobileica2/GameActivity.java
+++ app/src/main/java/tees/w9055613/mymobileica2/GameActivity.java
@@ -1,18 +1,29 @@
 package tees.w9055613.mymobileica2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity implements View.OnTouchListener {
+public class GameActivity extends AppCompatActivity implements
+        View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
+
     StringBuilder sb = new StringBuilder();
     GameView gameView;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,12 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
         gameView = new GameView(this);
         gameView.setOnTouchListener((View.OnTouchListener) this);
         setContentView(gameView); // <-- using gameView instead of a layout file
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(this, this);
+        // Set the gesture detector as the double tap listener
+        mDetector.setOnDoubleTapListener(this);
     }
 
     // TODO:
@@ -46,6 +63,12 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
         return true; // The event was consumed!
     }
 
+    /*@Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        if(this.mDetector.onTouchEvent(motionEvent))
+            return true;
+        return super.onTouchEvent(motionEvent);
+    }*/
 
     @Override
     protected void onResume() {
@@ -59,5 +82,55 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
         gameView.pause();
     }
 
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + motionEvent.toString());
+        return true;
+    }
 
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + motionEvent.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + motionEvent.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG,"onDown: " + motionEvent.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onShowPress: " + motionEvent.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + motionEvent.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent1, MotionEvent motionEvent2, float distanceX, float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + motionEvent1.toString() + motionEvent2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+        Log.d(DEBUG_TAG, "onLongPress: " + motionEvent.toString());
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + motionEvent1.toString() + motionEvent2.toString());
+        return true;
+    }
 }
